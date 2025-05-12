ef9942323c6266bc83073645dd6fe7dbd5233423
Muhtasib Ibtida Kousik <145317359+muhtasib-ibtida@users.noreply.github.com>
2025-05-12 22:59:11 +0600

    Add files via upload

---
+++ C++/01_Greeting_Message.cpp
@@ -0,0 +1,12 @@
+//Greeting Message: Write a program that takes a name as input and prints a greeting message, e.g., "Hello, [Name]!"
+#include<iostream>
+using namespace std;
+
+int main(){
+    string gret= "Hello Muhtasib Ibtida Kousik!";
+
+    cout<<gret<<endl;
+
+
+    return 0;
+}
