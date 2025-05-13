7ee641013c987f466ad0e8f614ec4016e2d3a9b7
Muhtasib Ibtida Kousik <145317359+muhtasib-ibtida@users.noreply.github.com>
2025-05-13 22:47:15 +0600

    Add files via upload

---
+++ C++/02_Add_Two_Numbers.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a = 10;
+    int b = 20;
+    string sum = "The summation is: ";
+
+
+    cout << sum << a+b;
+
+
+    return 0;
+}
