2a3a033d98cd58525b604055cd5c49c9958157e5
Muhtasib Ibtida Kousik <145317359+muhtasib-ibtida@users.noreply.github.com>
2025-05-18 21:53:18 +0600

    Add files via upload

---
+++ C++/09_Simple_Interest_Calculator.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int pa = 20;
+    int roi = 40;
+    int tiy = 60;
+    string si = "The simple interest is: ";
+
+    cout << si << (pa*roi*tiy)/100;
+
+
+    return 0;
+}
---
+++ C++/10_Square_and_Cube_of_a_Number.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a = 20;
+    string sq = "Square of the number is: ";
+    string cu = "Cube of the number is: ";
+
+    cout << sq << a*a << endl;
+    cout << cu << a*a*a << endl;
+
+
+    return 0;
+}
